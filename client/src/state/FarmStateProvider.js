e9bd18066b3243d5b6f0e61fc9d4ed769daf6284
Luke Lee <lslee714@gmail.com>
2021-04-30 22:32:18 -0600

    setup api call to get all farms

--- client/src/state/FarmStateProvider.js
+++ client/src/state/FarmStateProvider.js
@@ -1,23 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 import FarmStateContext from './FarmStateContext';
+import {GET_FARMS_URL} from "../urls";
 
 const FarmStateProvider = (props) => {
   const [farmsState, setFarmsState] = useState({
     farms: {},
   });
 
-  const loadFarms = (farms) => {
+
+  const loadFarms = useCallback((farms) => {
     setFarmsState({
       farms
     });
-  };
+  }, []);
+
+  const getFarms = useCallback( async () => {
+    const response = await fetch(GET_FARMS_URL);
+    const json = await response.json();
+    loadFarms(json);
+  }, [loadFarms]);
+
+  useEffect(() => {
+    if(!Object.keys(farmsState.farms).length) {
+      getFarms();
+    }
+  }, [farmsState.farms, getFarms]);
 
   return (
     <FarmStateContext.Provider value={
       {
         farms: farmsState.farms,
-        loadFarms,
       }
     }>
       {props.children}
