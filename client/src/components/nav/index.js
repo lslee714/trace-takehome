a7a04fc87884a425c7ef7aa88fa992faa915d5e7
Luke Lee <lslee714@gmail.com>
2021-04-30 21:28:02 -0600

    nav bar

---
+++ client/src/components/nav/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+
+import logo from "../../images/logo.svg";
+
+import './index.scss';
+
+const TraceNavComponent = () => {
+  return (
+    <Navbar>
+      <Navbar.Brand href="" className='nav'>
+        <img
+          alt="logo"
+          src={logo}
+          width="300"
+          height="50"
+          className="d-inline-block align-top"
+        />
+      </Navbar.Brand>
+    </Navbar>
+  )
+};
+
+export default TraceNavComponent;
