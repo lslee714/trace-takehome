c8820e0c0941748e704d78703ea328eb35ef97c7
Luke Lee <lslee714@gmail.com>
2021-04-30 22:55:23 -0600

    add documentation

--- client/src/components/farm-page/index.js
+++ client/src/components/farm-page/index.js
@@ -2,6 +2,10 @@ import FarmMapComponent from "../farm-map";
 import FarmStateProvider from "../../state/FarmStateProvider";
 
 
+//"Top level" component for the farms page
+//Allows us to wrap it in a specific provider
+//To better scope out and restrict our state
+//Which is a benefit (imo) of context api vs redux
 const FarmPageComponent = () => {
   return (
     <>
